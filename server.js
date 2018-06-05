891a5923b0d5d3e2e6f474efca91a7ac1d96b1b2
Carlos Lenon <code@carloslenon.com>
2018-06-04 19:18:00 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,26 +2,21 @@ import express from 'express';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import Models from './src/db/models';
-import Middleware from './src/common/security/http.middleware';
 import Routes from './src/endpoint';
 import path from 'path';
-import { general } from './src/common/config/auth.config';
 
 const app = express();
 
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(Middleware);
 app.use(Routes);
 
 const env = process.env.NODE_ENV || 'development';
 
 app.use(express.static(__dirname));
-app.get('/docs/flowdu-api/1.0.0', (req, res, next) => {
-    if(env === general.env.dev) {        
-        res.sendFile(path.join(__dirname + '/docs/index.html'));        
-    }
+app.get('/docs/node-seed-template/1.0.0', (req, res, next) => {
+    res.sendFile(path.join(__dirname + '/docs/index.html')); 
 });
 
 Models.sequelize.sync()
@@ -34,6 +29,6 @@ Models.sequelize.sync()
 
 function startApp(port) {
     app.listen(port, function(){
-        console.log('|o| yupiii!! flowdu api is now listening on port', port);
+        console.log('|o| yupiii!! your api seed is now listening on port', port);
     });
 }
